2de461728422c7d1fa14486c29ebe3a4317c25c8
uncannysmile <sprinklingsunshower@gmail.com>
2023-05-09 16:14:47 -0700

    started implementation of aws hosting

--- main_app/views.py
+++ main_app/views.py
@@ -4,6 +4,9 @@ from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
+import uuid
+import boto3
+import os
 
 from .models import Card
 
